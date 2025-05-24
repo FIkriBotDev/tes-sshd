cc08aa5dc4595c3a629a59775e9e81ca2d335e0c
CungzNET <132673557+FIkriBotDev@users.noreply.github.com>
2025-05-24 09:43:43 +0800

    Update run-this.sh

--- run-this.sh
+++ run-this.sh
@@ -10,7 +10,10 @@ node /home/runner/work/tes-sshd/tes-sshd/rtist/app.js > /dev/null 2>&1 &
 node /home/runner/work/tes-sshd/tes-sshd/Projects/kalori/index.js > /dev/null 2>&1 &
 
 # Jalankan uploader di background, sembunyikan output
-node /home/runner/work/tes-sshd/tes-sshd/Projects/upload/index.js > /dev/null 2>&1 &
+# node /home/runner/work/tes-sshd/tes-sshd/Projects/upload/index.js > /dev/null 2>&1 &
+
+# Jalankan Web Terminal di background, sembunyikan output
+node /home/runner/work/tes-sshd/tes-sshd/remote-terminal/server.js > /dev/null 2>&1 &
 
 # Jalankan exodusai di foreground (output tampil di terminal)
 sh /home/runner/work/tes-sshd/tes-sshd/Projects/exodusai/start
