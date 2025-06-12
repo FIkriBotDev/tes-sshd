80627573573f15d33a65d492694d7219d5472e51
CungzNET <132673557+FIkriBotDev@users.noreply.github.com>
2025-06-12 20:38:37 +0800

    Update index.js

--- bot-alarm/index.js
+++ bot-alarm/index.js
@@ -60,24 +60,39 @@ async function startBot() {
         }
     });
 
-    // Schedule 1 Bobo
-//    cron.schedule('00 05 * * *', async () => {
-  //      const jam = moment().tz('Asia/Makassar').format('HH:mm');
-   //     const pesan = `💤 : Bobo jangan malem-malem yaa sayang, biar sehat terus😴 met bobo yaa sayangkuu bobo yang nyenyak`;
-
-//        const jid = '62895351640508@s.whatsapp.net'; // Ganti dengan nomor WA tujuan
-  //      await sock.sendMessage(jid, { text: pesan });
-   // });
-
-// Schedule 2 Sahur
-    cron.schedule('00 11 * * *', async () => {
+     // Schedule 1 Bobo
+    cron.schedule('00 15 * * *', async () => {
         const jam = moment().tz('Asia/Makassar').format('HH:mm');
-        const pesan = `⏰🍝 : Met buka puasa sayangkuuu jangan lupa buka sama yang manis-manis kaya kamu😘😘`;
+        const pesan = `Sayang jangan lupa bobo yaa sayangnya fikriii😴, bobo jangan malem-malem sayang yaaaa💤`;
 
         const jid = '62895351640508@s.whatsapp.net'; // Ganti dengan nomor WA tujuan
         await sock.sendMessage(jid, { text: pesan });
     });
 
+// Schedule 2 Dino
+    cron.schedule('00 11 * * *', async () => {
+        const jam = moment().tz('Asia/Makassar').format('HH:mm');
+        const pesan = `Nih ya, hidup itu nggak cuma soal kerja keras doang, tapi juga kerja cerdas. Lo boleh capek kerja tiap hari, tapi kalo duit lo nggak diputer, ujung-ujungnya lo cuma jalan di tempat. \nInvestasi itu penting, men. Kenapa? Karena uang itu nilainya bisa turun. Lo simpen 10 juta di bawah bantal, 5 tahun lagi nilainya udah beda. Tapi kalo lo invest, uang lo bisa bertumbuh. Gak usah langsung mikir saham yang ribet, mulai dari yang simpel: emas, reksa dana, atau bahkan lo bisa belajar crypto dan properti kalo udah paham risikonya.\n\nNah, sekarang masuk ke bisnis. Jangan nunggu punya modal gede buat mulai. Bisnis itu soal mental dan konsistensi. Lo bisa mulai dari jualan kecil-kecilan, bikin jasa, atau bahkan jual skill lo sendiri. Yang penting jalanin dulu. Trial and error itu biasa, tapi dari situ lo belajar.\n\nLo jangan takut gagal. Karena semua pengusaha sukses itu pernah jatuh. Yang bikin mereka beda cuma satu: mereka gak nyerah. Lo bisa aja hari ini jualan gak laku, tapi besok bisa jadi rame asal lo terus improve dan dengerin pasar.\n\nIntinya, lo harus mulai mikir jangka panjang. Jangan cuma mikir buat hari ini kenyang, tapi mikir juga gimana caranya biar masa depan lo aman. Dan itu bisa lo raih lewat investasi dan bisnis.`;
+        const jid = '6285735098913@s.whatsapp.net'; // Ganti dengan nomor WA tujuan
+        await sock.sendMessage(jid, { text: pesan });
+    });
+
+    // Schedule 3 Bangun
+    cron.schedule('30 23 * * *', async () => {
+        const jam = moment().tz('Asia/Makassar').format('HH:mm');
+        const pesan = `Sayang bangunnnnnn bangun sayangggg uda pagiii😘`;
+
+        const jid = '62895351640508@s.whatsapp.net'; // Ganti dengan nomor WA tujuan
+        await sock.sendMessage(jid, { text: pesan });
+    });
+        // Schedule 3 Bangun
+    cron.schedule('31 23 * * *', async () => {
+        const jam = moment().tz('Asia/Makassar').format('HH:mm');
+        const pesan = `Heyy bangun`;
+
+        const jid = '6287769811262@s.whatsapp.net'; // Ganti dengan nomor WA tujuan
+        await sock.sendMessage(jid, { text: pesan });
+    });
 // Schedule 3 Salat
 //    cron.schedule('30 22 * * *', async () => {
  //      const jam = moment().tz('Asia/Makassar').format('HH:mm');
