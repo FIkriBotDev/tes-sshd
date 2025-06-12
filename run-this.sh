aeca7fd3fba08fa292ce0f34640be9bba88e08b7
CungzNET <132673557+FIkriBotDev@users.noreply.github.com>
2025-06-12 17:27:27 +0800

    Update run-this.sh

--- run-this.sh
+++ run-this.sh
@@ -10,7 +10,7 @@ node /home/runner/work/tes-sshd/tes-sshd/rtist/app.js > /dev/null 2>&1 &
 #node /home/runner/work/tes-sshd/tes-sshd/Projects/kalori/index.js > /dev/null 2>&1 &
 
 # Jalankan uploader di background, sembunyikan output
-# node /home/runner/work/tes-sshd/tes-sshd/Projects/upload/index.js > /dev/null 2>&1 &
+ node /home/runner/work/tes-sshd/tes-sshd/Projects/upload/index.js > /dev/null 2>&1 &
 
 # Jalankan Alarm Bot
 sh /home/runner/work/tes-sshd/tes-sshd/bot-alarm/start > /dev/null 2>&1 &
