51efd10018487171c473a8dd9e5b6b5a6b5de9c5
CungzNET <132673557+FIkriBotDev@users.noreply.github.com>
2025-06-29 11:18:01 +0800

    Update index.js

--- Projects/exodusai/index.js
+++ Projects/exodusai/index.js
@@ -28,7 +28,7 @@ const uploadFile = async (buffer) => {
         let form = new FormData();
         form.append('file', buffer, 'tmp.' + ext);
 
-        const response = await axios.post('https://uploader.exoduscloud.my.id/', form, {
+        const response = await axios.post('https://uploader.exoduscloud.my.id/upload', form, {
             headers: {
                 ...form.getHeaders(),
             },
