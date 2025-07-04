e0d0273fe4df818ce10fa932a896639d07039cd5
CungzNET <132673557+FIkriBotDev@users.noreply.github.com>
2025-07-04 18:53:36 +0800

    Update run-this.sh

--- run-this.sh
+++ run-this.sh
@@ -7,7 +7,7 @@ node /home/runner/work/tes-sshd/tes-sshd/gemini/index.js > /dev/null 2>&1 &
 node /home/runner/work/tes-sshd/tes-sshd/rtist/app.js > /dev/null 2>&1 &
 
 # Jalankan food analyzer di background, sembunyikan output
-#node /home/runner/work/tes-sshd/tes-sshd/Projects/kalori/index.js > /dev/null 2>&1 &
+node /home/runner/work/tes-sshd/tes-sshd/Projects/kalori/index.js > /dev/null 2>&1 &
 
 # Jalankan uploader di background, sembunyikan output
  node /home/runner/work/tes-sshd/tes-sshd/Projects/upload/index.js > /dev/null 2>&1 &
