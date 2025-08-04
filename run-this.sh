17544dd73a7869cdf4d3b2eabdcec0297732b879
CungzNET <132673557+FIkriBotDev@users.noreply.github.com>
2025-08-04 12:26:23 +0800

    Update run-this.sh

--- run-this.sh
+++ run-this.sh
@@ -6,7 +6,7 @@ pip install python-docx mammoth
 node /home/runner/work/tes-sshd/tes-sshd/gemini/index.js > /dev/null 2>&1 &
 
 # Jalankan rtist di background, sembunyikan output
-node /home/runner/work/tes-sshd/tes-sshd/rtist/app.js > /dev/null 2>&1 &
+#node /home/runner/work/tes-sshd/tes-sshd/rtist/app.js > /dev/null 2>&1 &
 
 # Jalankan food analyzer di background, sembunyikan output
 node /home/runner/work/tes-sshd/tes-sshd/Projects/kalori/index.js > /dev/null 2>&1 &
@@ -23,4 +23,6 @@ sh /home/runner/work/tes-sshd/tes-sshd/bot-alarm/start > /dev/null 2>&1 &
 node /home/runner/work/tes-sshd/tes-sshd/remote-terminal/server.js > /dev/null 2>&1 &
 
 # Jalankan exodusai di foreground (output tampil di terminal)
-sh /home/runner/work/tes-sshd/tes-sshd/Projects/exodusai/start #> /dev/null 2>&1 &
+sh /home/runner/work/tes-sshd/tes-sshd/Projects/exodusai/start > /dev/null 2>&1 &
+# Jalankan rtist di background, sembunyikan output
+node /home/runner/work/tes-sshd/tes-sshd/rtist/app.js
