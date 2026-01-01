0a86812058978e955d651b8a66a2a5be3103a1a5
CungzNET <132673557+FIkriBotDev@users.noreply.github.com>
2026-01-01 23:28:13 +0800

    Update run-this.sh

--- run-this.sh
+++ run-this.sh
@@ -29,7 +29,7 @@ npm install express
 cd /home/runner/work/tes-sshd/tes-sshd
 
 # Jalankan Invoice Bot
-sh /home/runner/work/tes-sshd/tes-sshd/Projects/invoice-saas-json-wa/start #> /dev/null 2>&1 &
+#sh /home/runner/work/tes-sshd/tes-sshd/Projects/invoice-saas-json-wa/start > /dev/null 2>&1 &
 
 # Jalankan Web Terminal di background, sembunyikan output
 node /home/runner/work/tes-sshd/tes-sshd/remote-terminal/server.js > /dev/null 2>&1 &
@@ -50,7 +50,7 @@ sh /home/runner/work/tes-sshd/tes-sshd/Projects/www-exodusai/start > /dev/null 2
 sh /home/runner/work/tes-sshd/tes-sshd/Projects/photo-to-pdf/start > /dev/null 2>&1 &
 
 # Jalankan code-server di background, sembunyikan output
-sh /home/runner/work/tes-sshd/tes-sshd/Projects/code-server/start > /dev/null 2>&1 &
+sh /home/runner/work/tes-sshd/tes-sshd/Projects/code-server/start #> /dev/null 2>&1 &
 
 # Jalankan exodusai di foreground (output tampil di terminal)
 sh /home/runner/work/tes-sshd/tes-sshd/Projects/exodusai/start #> /dev/null 2>&1 &
